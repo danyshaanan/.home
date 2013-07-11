8d01ec9a63be6d973b54ed4260e4e1ac155b3a0e
Dany Shaanan <danys@wix.com>
2013-07-11 03:14:41 -0400

    changed PS1

--- .zshrc.general.sh
+++ .zshrc.general.sh
@@ -4,7 +4,7 @@ if [ -f ~/.bashrc.general.sh ]; then
 fi
 
 
-PS1="$PR_YELLOW%n$PR_GREEN:$PR_RED%d $PR_NO_COLOR> "
+PS1="$PR_GREEN%n$PR_RED:$PR_GREEN%d$PR_RED>$PR_NO_COLOR "
 RPS1="$PR_BLUE%D:%*$PR_NO_COLOR"
 
 
