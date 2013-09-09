2219e913958166fc01b0bcbe5d50494899d31cda
Dany Shaanan <danys@wix.com>
2013-09-09 17:40:07 +0300

    removed trailing slashed from path

--- .bashrc.osx.sh
+++ .bashrc.osx.sh
@@ -4,7 +4,7 @@ if [[ `uname` != 'Darwin' ]]; then
 fi
 
 export NODE_PATH="/usr/local/bin/node"
-export PATH=~/bin/:/usr/local/bin:/usr/local/sbin/:/usr/local/mysql/bin:/usr/local/Cellar/ruby/1.9.3-p194/bin:/usr/local/share/npm/bin:$PATH
+export PATH=~/bin:/usr/local/bin:/usr/local/sbin:/usr/local/mysql/bin:/usr/local/Cellar/ruby/1.9.3-p194/bin:/usr/local/share/npm/bin:$PATH
 export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python2.7/site-packages/
 export PS1='\t:\u@\h:\w$(parse_git_branch)$ ' #TODO: check if to make linux as well #TODO: this is exported to root on su, while the funciton isn't
 export LSCOLORS='gxfxcxdxbxegedabagaced'
