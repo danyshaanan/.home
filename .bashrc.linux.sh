1e052749277ae187e5cd2fe249c8bdc67c497460
Dany <danyshaanan@gmail.com>
2013-10-26 14:27:47 +0200

    seperated truecrypt alias to osx and linux

--- .bashrc.linux.sh
+++ .bashrc.linux.sh
@@ -9,3 +9,4 @@ alias ll='ls -lh -F --color --group-directories-first'
 alias renrot='renrot -n "%Y%m%d%H%M%S-%C"'
 alias updg='sudo apt-get update && sudo apt-get upgrade'
 alias screenshot='import -display :0.0 -window root ~/screenshot_`d`_`t`.png' #by imagemagick.
+alias truecrypt='truecrypt -t'
