af38065f9521039bef574bd95d71965b9f5ee48a
Dany <danyshaanan@gmail.com>
2014-03-29 00:13:38 +0300

    added ntfs instructions

---
+++ installations/ntfs_on_osx.sh
@@ -0,0 +1,7 @@
+# http://apple.stackexchange.com/questions/106589/write-in-ntfs-using-mavericks
+brew update
+brew install osxfuse
+brew install ntfs-3g
+brew info osxfuse
+#run the two command lines outputed by last command, starting with "sudo /bin/cp" and "sudo chmod"
+#restart
