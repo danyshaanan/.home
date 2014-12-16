7d71c1950bea7338a0bac50d2082b73109d50348
Dany Shaanan <danyshaanan@gmail.com>
2014-12-16 20:12:20 +0200

    added updatebrewcask

--- bashrc.osx.sh
+++ bashrc.osx.sh
@@ -29,6 +29,7 @@ alias unmount='diskutil umount'
 alias wifi='osx-wifi-cli'
 alias mylocalip='ifconfig | grep -Eo "inet (addr:)?([0-9]*\.){3}[0-9]*" | grep -Eo "([0-9]*\.){3}[0-9]*" | grep -v "127."'
 alias eject='drutil eject' # remove when cds will finally die
+alias updatebrewcask='brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup'
 
 osxsleep() {
 	sleep `echo "scale=4; 60 * ${1:-0}" | bc` && pmset sleepnow
