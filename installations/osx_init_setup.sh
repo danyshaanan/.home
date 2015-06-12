b06465d48ce997427e0a7b9391235cade6371995
Dany Shaanan <danyshaanan@gmail.com>
2015-06-12 16:46:30 +0300

    added line to change background color

--- installations/osx_init_setup.sh
+++ installations/osx_init_setup.sh
@@ -14,6 +14,14 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+
+###############################################################################
+# New                                                                         #
+###############################################################################
+
+# Set background color to dark gray:
+osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Library/Desktop Pictures/Solid Colors/Solid Gray Dark.png"'
+
 ###############################################################################
 # General UI/UX                                                               #
 ###############################################################################
