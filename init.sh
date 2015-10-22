da752873deef9bf19853e1368586a6d362a202df
Dany Shaanan <danyshaanan@gmail.com>
2015-10-22 23:06:41 +0300

    Add .nvmrc

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for NAME in bash_profile bashrc inputrc screenrc vimrc editorconfig
+for NAME in bash_profile bashrc inputrc screenrc vimrc editorconfig nvmrc
 do
   DEST=$HOME/.$NAME
   [ -f $DEST ] && mv $DEST $DEST.moved.$(date +"%Y%m%d.%H%M%S")
