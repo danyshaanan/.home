da2161c971899a60cd6046f7a30f377d16d11d21
Dany Shaanan <danyshaanan@gmail.com>
2016-03-03 20:28:39 +0200

    Add gitconfig and test to check it is used

---
+++ bin/lib/configit.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+/* eslint brace-style:0 */
+'use strict'
+
+const p = (deps => {
+  try { return deps.reduce((o, r) => (o[r] = require(r), o), {}) }
+  catch(e) { throw new Error('Missing dependencies!') }
+})(['os', 'fs', 'ini', 'lodash', 'chalk'])
+
+const _ = p.lodash
+
+function getIni(path) {
+  return p.ini.parse(p.fs.readFileSync(path, 'utf-8'))
+}
+
+const expected = getIni('gitconfig')
+const actual = getIni(p.os.homedir() + '/.gitconfig')
+const diff = {}
+
+_.forIn(expected, (section, sectionName) => {
+  _.forIn(section, (expectedValue, key) => {
+    if (_.get(actual, [sectionName, key]) !== expectedValue) {
+      _.set(diff, [sectionName, key], expectedValue)
+    }
+  })
+})
+
+if (_.isEmpty(diff)) {
+  console.log(p.chalk.green('~/.gitconfig up to date!'))
+} else {
+  console.log(p.chalk.yellow('--------- Missing values in ~/.gitconfig ---------'))
+  console.log(p.ini.stringify(diff, { whitespace: true }).trim())
+  console.log(p.chalk.yellow('--------------------------------------------------'))
+  process.exit(1)
+}
