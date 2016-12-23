f0bdc42dcf6fe9df5d7ead6f62447e5de0299255
Dany Shaanan <danyshaanan@gmail.com>
2016-12-23 11:51:28 +0200

    Extract the square brackets from git-promot to PS1

--- bashrc/ps1.sh
+++ bashrc/ps1.sh
@@ -4,7 +4,7 @@
 # \h is comp
 # \w is working directory
 
-which git-prompt > /dev/null 2> /dev/null && PS1GITPROMPT='$(git-prompt)'
+which git-prompt > /dev/null 2> /dev/null && PS1GITPROMPT='[$(git-prompt)]'
 which node       > /dev/null 2> /dev/null && PS1NODEVERSION='[$(node -v | sed "s/^v//" | sed "s/\.[0-9]\{1,\}$//")] '
 
 PS1='\t '$PS1NODEVERSION$PS1GITPROMPT' \w $ '
