1fb56434ea1bccfdc7ca42d2f85e7a81860ac3f0
Dany Shaanan <danyshaanan@gmail.com>
2017-03-30 10:41:53 +0300

    Add fzf copy commands

--- bashrc/fzf.sh
+++ bashrc/fzf.sh
@@ -5,3 +5,5 @@ export FZF_DEFAULT_COMMAND='ag -g ""'
 
 ff (){ command $@           $(fzf) ; }
 fd (){ command $@ $(dirname $(fzf)); }
+ffc (){           fzf  | tr '\n' ' ' | pbcopy; }
+fdc (){ dirname $(fzf) | tr '\n' ' ' | pbcopy; }
