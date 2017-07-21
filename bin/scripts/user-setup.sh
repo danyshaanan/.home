0bc762fcbeb7bf0c606709729783043b1da8ccbe
Dany Shaanan <danyshaanan@gmail.com>
2017-07-21 14:44:35 +0300

    Disable java update notifications

--- bin/scripts/user-setup.sh
+++ bin/scripts/user-setup.sh
@@ -2,3 +2,6 @@
 
 echo "Disabling OSX's log of everything you download..."
 ln -fs /dev/null ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV2
+
+echo "Disabling Java's auto-update notifications..."
+sudo defaults write /LibraryPreferences/com.oracle.java.Java-Updater JavaAutoUpdateEnabled -bool false
