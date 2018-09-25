8be67fa846592f476279c4b1a483bfc76313207b
Dany Shaanan <danyshaanan@gmail.com>
2018-09-25 19:01:53 +0300

    Remove lodash

--- bin/lib/configit.js
+++ bin/lib/configit.js
@@ -10,9 +10,7 @@ const p = (deps => {
   } catch (e) {
     throw new Error('Missing dependencies!')
   }
-})(['os', 'fs', 'ini', 'lodash', 'chalk'])
-
-const _ = p.lodash
+})(['os', 'fs', 'ini', 'chalk'])
 
 function getIni(path) {
   return p.ini.parse(p.fs.readFileSync(path, 'utf-8'))
@@ -21,16 +19,18 @@ function getIni(path) {
 const expected = getIni(`${__dirname}/../../gitconfig`)
 const actual = getIni(`${p.os.homedir()}/.gitconfig`)
 const diff = {}
-
-_.forIn(expected, (section, sectionName) => {
-  _.forIn(section, (expectedValue, key) => {
-    if (_.get(actual, [sectionName, key]) !== expectedValue) {
-      _.set(diff, [sectionName, key], expectedValue)
+Object.keys(expected).forEach(sectionName => {
+  const section = expected[sectionName]
+  Object.keys(section).forEach(key => {
+    const expectedValue = section[key]
+    if (actual[sectionName] && actual[sectionName][key] !== expectedValue) {
+      diff[sectionName] = diff[sectionName] || {}
+      diff[sectionName][key] = expectedValue
     }
   })
 })
 
-if (_.isEmpty(diff)) {
+if (Object.keys(diff).length === 0) {
   console.log(p.chalk.green('~/.gitconfig up to date!'))
 } else {
   console.log(p.chalk.yellow('--------- Missing values in ~/.gitconfig ---------'))
