048e5d098bcbe929a3474943e96d2b2ee6ca5ce2
Dany Shaanan <danyshaanan@gmail.com>
2018-12-04 12:04:32 +0200

    Made quick html viewer recursive and smarter

--- bin/lib/see.js
+++ bin/lib/see.js
@@ -1,14 +1,46 @@
 #!/usr/bin/env node
 'use strict'
 
-const fs = require('fs')
-const cp = require('child_process')
+const { exec } = require('child_process')
+const { lstatSync, readdirSync, writeFileSync } = require('fs')
 
-const dir = process.cwd()
-const filter = f => ['png', 'jpg', 'jpeg', 'gif'].includes(f.split('.').pop().toLowerCase())
-const img = f => `<a href=${dir}/${f}><img src=${dir}/${f} height=200/></a>`
-const get = (p, t) => fs.writeFileSync(p, t) + cp.exec(`open ${p}`)
+const tree = p => ({ path: p, children: lstatSync(p).isDirectory() && readdirSync(p).map(c => tree(`${p}/${c}`)) })
+const get = (p, t) => writeFileSync(p, t) + exec(`open ${p}`)
 
-cp.exec('echo $TMPDIR', (e, r) => {
-  get(`${r.trim()}${Date.now()}.htm`, fs.readdirSync(dir).filter(filter).map(img).join(`\n`))
-})
+const html = `
+<body style='font-family: monospace; margin-bottom: 1000px;'>
+<div id=list></div><hr/>
+<div id=grid></div><hr/>
+</body>
+
+<script>
+'use strict'
+const tree = ${JSON.stringify(tree(process.cwd()))}
+
+const isDir = node => node.children
+const isImage = node => !isDir(node) && /\.(jpe?g|png|gif)$/i.test(node.path)
+const dir = node => '<a onClick=set("PATH")>PATH</a><br/>'.replace(/PATH/g, node.path)
+const img = node => '<a href=PATH><img src="PATH" height=200/></a>'.replace(/PATH/g, node.path)
+
+const dictionary = {}
+const list = document.getElementById('list')
+const set = path => document.getElementById('grid').innerHTML = dictionary[path]
+
+const add = node => {
+  dictionary[node.path] = node.children.filter(isImage).map(img).join(' ')
+  list.innerHTML += dir(node)
+  node.children.filter(isDir).forEach(add)
+}
+
+add(tree)
+set(tree.path)
+
+</script>
+`
+
+exec('echo $TMPDIR', (e, r) => get(`${r.trim()}viewer.${Date.now()}.htm`, html))
+
+
+
+// console.log(JSON.stringify(tree(dir), 0, 2))
+// console.log(tree(dir))
