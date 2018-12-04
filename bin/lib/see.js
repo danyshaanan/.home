2ead564691d03caac386c508acdc4da2541a8602
Dany Shaanan <danyshaanan@gmail.com>
2018-12-05 00:11:04 +0200

    Various changes to 'see'

--- bin/lib/see.js
+++ bin/lib/see.js
@@ -1,46 +1,10 @@
 #!/usr/bin/env node
 'use strict'
 
-const { exec } = require('child_process')
-const { lstatSync, readdirSync, writeFileSync } = require('fs')
+const fs = require('fs')
+const tree = p => ({ path: p, children: fs.lstatSync(p).isDirectory() && fs.readdirSync(p).map(c => tree(`${p}/${c}`)) })
+const html = `${fs.readFileSync(`${__dirname}/see.htm`)}`.replace('tree', JSON.stringify(tree(process.cwd())))
+const path = `${process.env.TMPDIR}viewer.${Date.now()}.htm`
 
-const tree = p => ({ path: p, children: lstatSync(p).isDirectory() && readdirSync(p).map(c => tree(`${p}/${c}`)) })
-const get = (p, t) => writeFileSync(p, t) + exec(`open ${p}`)
-
-const html = `
-<body style='font-family: monospace; margin-bottom: 1000px;'>
-<div id=list></div><hr/>
-<div id=grid></div><hr/>
-</body>
-
-<script>
-'use strict'
-const tree = ${JSON.stringify(tree(process.cwd()))}
-
-const isDir = node => node.children
-const isImage = node => !isDir(node) && /\.(jpe?g|png|gif)$/i.test(node.path)
-const dir = node => '<a onClick=set("PATH")>PATH</a><br/>'.replace(/PATH/g, node.path)
-const img = node => '<a href=PATH><img src="PATH" height=200/></a>'.replace(/PATH/g, node.path)
-
-const dictionary = {}
-const list = document.getElementById('list')
-const set = path => document.getElementById('grid').innerHTML = dictionary[path]
-
-const add = node => {
-  dictionary[node.path] = node.children.filter(isImage).map(img).join(' ')
-  if (dictionary[node.path]) list.innerHTML += dir(node)
-  node.children.filter(isDir).forEach(add)
-}
-
-add(tree)
-document.querySelector('a').click()
-
-</script>
-`
-
-exec('echo $TMPDIR', (e, r) => get(`${r.trim()}viewer.${Date.now()}.htm`, html))
-
-
-
-// console.log(JSON.stringify(tree(dir), 0, 2))
-// console.log(tree(dir))
+fs.writeFileSync(path, html)
+require('child_process').exec(`open ${path}`)
