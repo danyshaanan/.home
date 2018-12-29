caa214af9d3c4f11f5c963cfc026986868d40720
Dany Shaanan <danyshaanan@gmail.com>
2018-12-29 12:22:29 +0200

    Set 'dupes' to not write files

--- bashrc/sort/general.sh
+++ bashrc/sort/general.sh
@@ -60,6 +60,7 @@ alias tolowercase='rename y/A-Z/a-z/' #renames target files to lowercase
 alias ___removebefore='rename s/.*___//g'
 alias ___removeafter='rename s/___.*\\././g'
 alias updatenode='nvm install node --reinstall-packages-from=node'
+alias dupes='dupes --no-write'
 nametab () { export PROMPT_COMMAND='echo -ne "\033]0;'$*'\007"'; } # { printf "\e]1;$*\a"; }
 lsj () { node -e 'console.log(JSON.stringify(require("fs").readdirSync("'$1'" || "."),0,2))'; }
 insert () { mkdir $1; mv $1* $1; rmdir $1; }
