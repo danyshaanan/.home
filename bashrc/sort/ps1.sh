df4379abcbe18786155b7306448087c5a41375b7
Dany Shaanan <danyshaanan@gmail.com>
2021-08-08 19:36:21 +0300

    Add computer temp in PS1

--- bashrc/sort/ps1.sh
+++ bashrc/sort/ps1.sh
@@ -7,5 +7,11 @@
 which git-prompt > /dev/null 2> /dev/null && PS1GITPROMPT='[$(git-prompt)] '
 which node       > /dev/null 2> /dev/null && PS1NODEVERSION='[$(node -v | sed "s/^v//")] '
 which ruby       > /dev/null 2> /dev/null && PS1RUBYVERSION='[$(ruby -v | sed "s/^ruby //" | sed "s/ .*//")] '
+cat /tmp/.tempc  > /dev/null 2> /dev/null && PS1TEMPC='[$(cat /tmp/.tempc | tr -d "\n")°] '
 
-PS1="\t $PS1NODEVERSION$PS1GITPROMPT\w> "
+PS1="\t $PS1TEMPC$PS1GITPROMPT\w> "
+
+
+# Add to root crontab:
+# * * * * * powermetrics --samplers smc -n 1 |grep -i "CPU die temperature" | grep -o "\d\+\.\d\+" > /tmp/.tempc
+
