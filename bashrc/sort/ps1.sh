10a6cffc46741ce7d81072125dafdbe5126d34e3
Dany Shaanan <danyshaanan@gmail.com>
2021-08-09 12:00:55 +0300

    Refactor PS1

--- bashrc/sort/ps1.sh
+++ bashrc/sort/ps1.sh
@@ -4,12 +4,12 @@
 # \h is comp
 # \w is working directory
 
-which git-prompt > /dev/null 2> /dev/null && PS1GITPROMPT='[$(git-prompt)] '
-which node       > /dev/null 2> /dev/null && PS1NODEVERSION='[$(node -v | sed "s/^v//")] '
-which ruby       > /dev/null 2> /dev/null && PS1RUBYVERSION='[$(ruby -v | sed "s/^ruby //" | sed "s/ .*//")] '
-cat /tmp/.tempc  > /dev/null 2> /dev/null && PS1TEMPC='[$(cat /tmp/.tempc | tr -d "\n")°] '
+which git-prompt > /dev/null 2> /dev/null && PS1GITPROMPT='$(git-prompt)'
+which node       > /dev/null 2> /dev/null && PS1NODEVERSION='[$(node -v | sed "s/^v//")]'
+which ruby       > /dev/null 2> /dev/null && PS1RUBYVERSION='[$(ruby -v | sed "s/^ruby //" | sed "s/ .*//")]'
+cat /tmp/.tempc  > /dev/null 2> /dev/null && PS1TEMPC='[$(cat /tmp/.tempc | tr -d "\n")°]'
 
-PS1="\t $PS1TEMPC$PS1GITPROMPT\w> "
+PS1="\t $PS1TEMPC$PS1GITPROMPT \w> "
 
 
 # Add to root crontab:
